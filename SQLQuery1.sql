b01761d5932061ff5a0ae427a1a317ede6d57c29
sladjantasic <sladjan.m.tasic@gmail.com>
2020-07-13 13:59:54 +0200

    hide connection string

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE Devices (
     [Id]          INT            NOT NULL PRIMARY KEY,
-    [Name]        NVARCHAR (30)  NOT NULL,
+    [Name]        NVARCHAR (30)  NULL,
     [Description] NVARCHAR (300) NULL,
     [IsConnected] BIT            NOT NULL,
-    [UserId]      NVARCHAR (450) NULL
+    [UserId]      NVARCHAR (450) NULL,
 )
 GO
--- SladjanCMSAzure/Models/SQLdbContext.cs
+++ SladjanCMSAzure/Models/SQLdbContext.cs
@@ -21,8 +21,7 @@ namespace SladjanCMSAzure.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                //To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:sladjansql.database.windows.net,1433;Initial Catalog=SQLdb;Persist Security Info=False;User ID=dbAdmin;Password=Admin123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings.SqlConnection");
             }
         }
 
